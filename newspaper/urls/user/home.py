b744a99bafb6a3249f95dd68b1d082496fc31ccb
saraivaufc <saraiva.ufc@gmail.com>
2015-03-14 13:46:03 -0300

    atualizando os templates

--- newspaper/urls/user/home.py
+++ newspaper/urls/user/home.py
@@ -3,5 +3,6 @@ from newspaper.views.user import *
 
 urlpatterns = patterns('',
 	url(r'^$', home),
-	url(r'^(?P<id_subsection>\d+)/$', home),
+	url(r'^(?P<id_section>\d+)/$', home),
+	url(r'^(?P<id_section>\d+)/(?P<id_subsection>\d+)/$', home),
 )
--- newspaper/views/user/home.py
+++ newspaper/views/user/home.py
@@ -2,14 +2,25 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from newspaper.models import Section, News, Classifield
 from datetime import datetime
+from newspaper.utils import getNewsFromSection
 
-def home(request, id_subsection=None):
+def home(request, id_section=None, id_subsection=None):
 	sections = []
 	sections = Section.objects.filter()
-	if id_subsection != None:
-		news_all = News.objects.filter(subsection = id_subsection).order_by('-dating_news')
+	if id_section != None:
+		section = Section.objects.get(id = int(id_section))
+		news_filter = getNewsFromSection(section)
 	else:
-		news_all = News.objects.filter().order_by('-dating_news')
+		news_filter = News.objects.filter().order_by('-dating_news')
+	if id_subsection != None:
+		news_all = []
+		for i in news_filter:
+			print i.subsection_id,id_subsection 
+			if i.subsection_id == int(id_subsection):
+				news_all.append(i)
+	else:
+		news_all = news_filter		
+
 	news_image = []
 	news_no_image = []
 	most_popular = []
